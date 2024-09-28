aba24946d761facf2ccbcbdc6b6214bf06cc7e72
vpreseault <vincent.preseault@gmail.com>
2024-09-28 10:08:54 -0400

    backend scaffold

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/vpreseault/hack-the-hill-2/backend
+
+go 1.22.2
---
+++ backend/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleRoot())
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Backend server running on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func handleRoot() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		response := `
+		<html>
+		<body>
+			<h1>Hello World</h1>
+		</body>
+		</html>
+		`
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(response))
+		if err != nil {
+			http.Error(w, "Unable to write response", http.StatusInternalServerError)
+		}
+	}
+}
