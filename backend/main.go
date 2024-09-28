f4ec26e21a3c0ddbe17bb1537940582099a3fcdb
vpreseault <vincent.preseault@gmail.com>
2024-09-28 10:17:41 -0400

    dynamic sessionId handler

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,6 +11,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot())
+	mux.HandleFunc("/{sessionId}", handleSession())
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -37,3 +39,25 @@ func handleRoot() http.HandlerFunc {
 		}
 	}
 }
+
+func handleSession() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sessionIDPathValue := r.PathValue("sessionId")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		template := `
+		<html>
+		<body>
+			<h1>Session ID: %s</h1>
+		</body>
+		</html>
+		`
+
+		response := fmt.Sprintf(template, sessionIDPathValue)
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(response))
+		if err != nil {
+			http.Error(w, "Unable to write response", http.StatusInternalServerError)
+		}
+	}
+}
+
