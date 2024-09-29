a57493ffdf69874ae9972c9d010f2131cf837474
vpreseault <vincent.preseault@gmail.com>
2024-09-28 21:18:01 -0400

    fix for real?

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -6,6 +6,6 @@ import (
 
 func Root() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./static/index.html")
+		http.ServeFile(w, r, "../frontend/static/index.html")
 	}
 }
