6509267151de6aad096e14bb519dc3d9b34fe531
vpreseault <vincent.preseault@gmail.com>
2024-09-29 08:35:03 -0400

    fix html file location and import path

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -1,23 +1,11 @@
 package handlers
 
 import (
-	"html/template"
 	"net/http"
-	"path/filepath"
 )
 
 func Root() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles(filepath.Join(".", "frontend", "index.html"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = tmpl.Execute(w, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		renderTemplate(w, "index")
 	}
 }
